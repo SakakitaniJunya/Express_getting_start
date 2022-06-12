6bbfe71bbc8de0a4563d5e9daa8010428a067817
sakaki <sakajun02080@gmail.com>
2022-06-12 13:29:30 +0900

    json error happen

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ var photoList = [
     {
         id: "001",
         name: "photo001.jpg",
-        type: "jpg",
+        type: "jpeg",
         dataUrl: "http://localhost:3000/data/photo001.jpeg"
     },
     {
-        id:"002",
-        name:"photo001.jpg",
-        type: "jpg",
+        id: "002",
+        name: "photo002.jpg",
+        type: "jpeg",
         dataUrl: "http://localhost:3000/data/photo002.jpeg"
     }
 ]
@@ -27,9 +27,18 @@ var photoList = [
 app.get("/api/photo/:photoId", function(req, res, next){
     var photo;
     for (i = 0;i < photoList.length; i++) {
-        var photo = photoList[i];
+        if(photoList[i].id == req.params.photoId){
+            var photo = photoList[i];
+        }
     }
-
     res.json(photo);
 });
+
+
+// View EngineにEJSを指定
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res, next) {
+    res.render("index", {});
+})
 
