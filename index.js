cb613b61f0902f7f38eb9938be6617957ce257e8
sakaki <sakajun02080@gmail.com>
2022-06-14 08:16:05 +0900

    20220614 commit

--- index.js
+++ index.js
@@ -15,21 +15,34 @@ var photoList = [
         name: "photo001.jpg",
         type: "jpg",
         dataUrl: "http://localhost:3000/data/photo001.jpeg"
-    },
-    {
+    },{
         id:"002",
-        name:"photo001.jpg",
+        name:"photo002.jpg",
         type: "jpg",
         dataUrl: "http://localhost:3000/data/photo002.jpeg"
     }
 ]
 
+// 写真リストを取得するAPI
+app.get("/api/photo/list", function(req, res, next){
+    res.json(photoList);
+});
+
 app.get("/api/photo/:photoId", function(req, res, next){
     var photo;
-    for (i = 0;i < photoList.length; i++) {
-        var photo = photoList[i];
+    for (i = 0; i < photoList.length; i++){
+        if (photoList[i].id == req.params.photoId){
+            var photo = photoList[i];
+        }
     }
-
     res.json(photo);
 });
 
+
+// View EngineにEJSを指定。
+app.set('view engine', 'ejs');
+
+// "/"へのGETリクエストでindex.ejsを表示する。拡張子（.ejs）は省略されていることに注意。
+app.get("/", function(req, res, next){
+    res.render("index", {});
+});
